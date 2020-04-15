d278f78df46ceea9073c2193b5c845a4dfbeb80a
Brooke Rose Baer <brooke.baer@pm.me>
2020-04-15 15:52:18 -0400

    solve Fib (dynamic programming)

--- exercises/Fib/index.js
+++ exercises/Fib/index.js
@@ -12,7 +12,13 @@
  */
 
 function fib(n) {
-
+    var f = new Array(n+2);
+    f[0] = 0;
+    f[1] = 1;
+    for (i = 2; i <= n; i++){
+        f[i] = f[i-1]+f[i-2];
+    }
+    return f[n];
 }
 
 module.exports = fib;
